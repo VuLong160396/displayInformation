b58aecca950276a3118f6727da64af64fc542e33
7-longvv <7-longvv@phenikaa.local>
2021-08-06 19:30:42 +0700

    add file thong tin ca nhan

---
+++ displayInformation.py
@@ -0,0 +1,10 @@
+Ho_va_ten = 'Vu Van Long'
+Nam_sinh = '1996'
+Tuoi = 2021 - int(Nam_sinh)
+Nghe_nghiep = 'Ky Su Dien Tu'
+Que_quan = 'HaiPhong City'
+print ('Toi ten la',Ho_va_ten)
+print ('Sinh nam',Nam_sinh)
+print ('Toi nam nay',Tuoi,'tuoi')
+print ('Hien nay toi dang lam',Nghe_nghiep)
+print ('Que huong cua toi la',Que_quan)
